aa5fd149c88ea63a87c32023668319e598610076
Jeong <jongseongj@gmail.com>
2025-07-06 23:18:35 +0900

    auto sync

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// src/pages/_app.tsx
+import "@/styles/globals.css"; // ✅ 이 위치에서만 import해야 함
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
