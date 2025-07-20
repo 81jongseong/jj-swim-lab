961c6d95f4709d4ec5b0c7c34b2eac9fd61781b5
Jeong <jongseongj@gmail.com>
2025-07-20 15:26:05 +0900

    💾 임시 커밋

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+// client/app/page.tsx
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-blue-500 text-white text-center text-3xl flex items-center justify-center">
+      Tailwind CSS 작동 확인 완료 🎉
+    </main>
+  );
+}
