1c95e569caa7b779c844527e14dea9ede021fa87
jongseongj <jongseongj@gmail.com>
2025-10-05 00:00:53 +0900

    理쒓퀬愿由ъ옄 ?뱁럹?댁? ?뚯뒪???꾨즺 - InstructorManagementPage 而댄룷?뚰듃 ?ㅻ쪟 ?섏젙

--- client/components/ui/index.ts
+++ client/components/ui/index.ts
@@ -121,7 +121,7 @@ export { default as PoseComparisonViewer } from './posecomparisonviewer';
 // 고급 UI 컴포넌트들
 export { default as LazyComponent } from './lazycomponent';
 export { default as OptimizedImage } from './optimizedimage';
-export { Progress as progress } from './progress';
+export { Progress } from './progress';
 export { Checkbox } from './checkbox';
 export { Alert, AlertTitle, AlertDescription } from './alert';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
